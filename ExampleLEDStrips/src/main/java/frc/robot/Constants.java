b247b15c63d784c357a03b2c396ddc7732281857
rachitkakkar <rachitkakkar7@gmail.com>
2024-01-11 20:14:58 -0600

    Add LED Subsystem Example

--- ExampleLEDStrips/src/main/java/frc/robot/Constants.java
+++ ExampleLEDStrips/src/main/java/frc/robot/Constants.java
@@ -4,16 +4,10 @@
 
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
+import org.lasarobotics.led.LEDStrip;
+
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+  public static class LEDHardware {
+    public static final LEDStrip.ID LED_STRIP_ID = new LEDStrip.ID("LEDHardware/Strip", 2, 30);
   }
 }
--- ExampleLEDStrips/src/main/java/frc/robot/RobotContainer.java
+++ ExampleLEDStrips/src/main/java/frc/robot/RobotContainer.java
@@ -4,29 +4,28 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import org.lasarobotics.led.LEDStrip;
+import org.lasarobotics.led.LEDSubsystem;
+import org.lasarobotics.led.LEDStrip.Pattern;
+import org.lasarobotics.led.LEDStrip.Section;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private static final LEDSubsystem LED_SUBSYSTEM = LEDSubsystem.getInstance();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
+    // Led Example
+    LEDStrip strip = new LEDStrip(LEDStrip.initializeHardware(null));
+    strip.set(Pattern.BLUE_SOLID);
+    strip.set(Pattern.ORANGE_WAVE, Section.MIDDLE);
+    LED_SUBSYSTEM.add(strip);
   }
 
   /**
